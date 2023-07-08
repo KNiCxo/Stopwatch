443566b3f8629c8f92e68018c81f82e5447b353b
KNiC.xo <ntcaliwag@hotmail.com>
2023-07-08 02:10:59 -0700

    Stopwatch can now be paused

--- stopwatch.js
+++ stopwatch.js
@@ -9,42 +9,57 @@ const additionalClock = document.querySelector('.js-additional-clock');
 
 // Button elemenents for stopwatch
 const startButtonElement = document.querySelector('.js-start-button');
-const stopButtonElement = document.querySelector('.js-stop-button');
+const stopButtonElement = document.querySelector('.js-reset-button');
 
-// Used to prevent stopwatch function from running if already running
+// Used to check if stopwatch is running
 let isOn = false;
+
+// Keeps ID of setInterval function for clearing
+let intervalID;
+
+// Stores total time elapsed for when stopwatch is paused
+let timeBank = 0;
+
+// Stores time elapsed while stopwatch is running
+let timeElapsed = 0;
 
 // Stopwatch function
 const stopwatch = () => {
-  // If stopwatch is not running, then continue
+  // Get current time to compare between each interval
+  let startTime = Date.now();
+
+ // If stopwatch is not running, make the start button say 'Stop' and begin the stopwatch function
   if (!isOn) {
     isOn = true;
-
-    // Get current time to compare between each interval
-    let startTime = Date.now();
-    let timeElapsed = 0;
+    startButtonElement.innerHTML = 'Stop';
 
     // Gets time elapsed every 1ms and converts to clock format
-    setInterval(() => {
+    intervalID = setInterval(() => {
       timeElapsed = Date.now() - startTime;
 
       // Divides time elapsed by measurement of time and uses modulo to maintain
       // a single digit per element. Used floor to remove decimal.
-      digitOne.innerHTML = Math.floor(timeElapsed / 10) % 10; // 10ms
-      digitTwo.innerHTML = Math.floor(timeElapsed / 100) % 10; // 100ms
-      digitThree.innerHTML = Math.floor(timeElapsed / 1000) % 10; // 1 second
-      digitFour.innerHTML = Math.floor(timeElapsed / 10000) % 6; // 10 seconds
-      digitFive.innerHTML = Math.floor(timeElapsed / 60000) % 10; // 1 minute
-      digitSix.innerHTML = Math.floor(timeElapsed / 600000) % 6; // 10 minutes
+      digitOne.innerHTML = Math.floor((timeBank + timeElapsed) / 10) % 10; // 10ms
+      digitTwo.innerHTML = Math.floor((timeBank + timeElapsed) / 100) % 10; // 100ms
+      digitThree.innerHTML = Math.floor((timeBank + timeElapsed) / 1000) % 10; // 1 second
+      digitFour.innerHTML = Math.floor((timeBank + timeElapsed) / 10000) % 6; // 10 seconds
+      digitFive.innerHTML = Math.floor((timeBank + timeElapsed) / 60000) % 10; // 1 minute
+      digitSix.innerHTML = Math.floor((timeBank + timeElapsed) / 600000) % 6; // 10 minutes
 
       // If time has elapsed to an hour, adds additionals clock elements
       if (timeElapsed > 3600000) {
         additionalClock.innerHTML = `
-          <span>${Math.floor(timeElapsed / 3600000)}</span>
+          <span>${Math.floor((timeBank + timeElapsed) / 3600000)}</span>
           <span>:</span> 
         `
       }
     }, 1);
+  } else {
+    // Else, turn off timer, store time elapsed in timeBank variable and make the stop button say 'Start'
+    isOn = false;
+    timeBank += timeElapsed;
+    clearInterval(intervalID);
+    startButtonElement.innerHTML = 'Start';
   }
 }
 
